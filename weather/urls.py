8a828fdf27bc3dde943a435b921461f728a95c49
tamaraali2001 <tamara.ali.mte20@uoitc.edu.iq>
2025-01-26 17:26:36 +0300

    delete

--- weather/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('get_countries/', views.get_countries, name='get_countries'),
-    path('get_provinces/', views.get_provinces, name='get_provinces'),
-    path('get_weather/', views.get_weather, name='get_weather'),
-]

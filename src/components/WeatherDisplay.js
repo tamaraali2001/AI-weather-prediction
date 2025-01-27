8ffb8c97043248c5d5235fa02b4e01139010b6bb
tamaraali2001 <tamara.ali.mte20@uoitc.edu.iq>
2025-01-27 05:57:52 +0300

    تعديل ملف البيانات

--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -19,13 +19,13 @@ function WeatherDisplay({ selectedRows, selectedCountry, selectedCity, onBack })
             <h3>{row.date}</h3>
             <p>{t('maxtemp_c')}: {Number(row.maxtemp_c).toFixed(2)}°C</p>
             <p>{t('mintemp_c')}: {Number(row.mintemp_c).toFixed(2)}°C</p>
-            <p>{t('Predicted Avg Temp')}: {Number(row['Predicted Avg Temp']).toFixed(2)}°C</p>
+            <p>{t('predicted_avgtemp_c')}: {Number(row.predicted_avgtemp_c).toFixed(2)}°C</p>
             <p>{t('temp_range')}: {Number(row.temp_range).toFixed(2)}°C</p>
             <p>{t('humidity')}: {Number(row.avghumidity).toFixed(2)}%</p>
             <p>{t('wind')}: {Number(row.maxwind_mph).toFixed(2)} mph</p>
             <p>{t('precip')}: {Number(row.totalprecip_mm).toFixed(2)} mm</p>
             <p>{t('visibility')}: {Number(row.avgvis_km).toFixed(2)} km</p>
-            <p>{t('uvIndex')}: {Number(row.uv_index).toFixed(2)}</p>
+            <p>{t('condition_text')}: {row.condition_text}</p>
           </div>
         ))}
       </div>
